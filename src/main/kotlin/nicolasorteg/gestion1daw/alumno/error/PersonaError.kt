22c0e39025bac737967036d3edd6fcc45d584d18
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-16 17:30:12 +0200

    Listado de errores hecho

---
+++ src/main/kotlin/nicolasorteg/gestion1daw/alumno/error/PersonaError.kt
@@ -0,0 +1,44 @@
+package nicolasorteg.gestion1daw.alumno.error
+
+/**
+ * Clase sellada que almacena todos los tipos de
+ * errores que se podría dar en el programa respecto a Persona.
+ */
+sealed class PersonaError(val message: String) {
+    /**
+     * Almacena el error que se mostrará en el validador.
+     *
+     * @param message Mensaje de error.
+     */
+    class PersonaValidatorError(message: String): PersonaError(message)
+
+    /**
+     * Almacena el error que se mostrará cuando no se encuentre el ID.
+     *
+     * @param id Identificador personal de la persona que no se ha podido encontrar.
+     *
+     */
+    class PersonaIdNotFound(id: Int): PersonaError("Persona no encontrada con id: $id")
+
+    /**
+     * Almacena el error que se mostrará cuando el storage del programa no encuentre el archivo.
+     *
+     * @param message Mensaje de error.
+     */
+    class PersonaStorageError(message: String): PersonaError(message)
+
+    /**
+     *  Almacena el error que se mostrará cuando la base de datos de la persona no se haya conectado.
+     *
+     *  @param message Mensaje de error.
+     */
+    class PersonaDatabaseException(message: String): PersonaError(message)
+
+    /**
+     * Almacenaa el error que se mostrara en el servicio cuando haya algún error.
+     *
+     * @param message Mensaje de error.
+     */
+
+    class PersonaServiceException(message: String): PersonaError(message)
+}
