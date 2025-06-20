a75a5367d523bcde19c0cede6a3ba9ad8abd93f0
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-20 23:26:35 +0200

    añado la llamada a la siguiente ventana

--- src/main/kotlin/nicolasorteg/gestion1daw/controllers/RegisterController.kt
+++ src/main/kotlin/nicolasorteg/gestion1daw/controllers/RegisterController.kt
@@ -1,14 +1,47 @@
 package nicolasorteg.gestion1daw.controllers
 
 import javafx.fxml.FXML
-import javafx.scene.control.Label
+import javafx.scene.control.PasswordField
+import javafx.scene.control.TextField
+import javafx.scene.control.Alert
+import javafx.scene.control.Alert.AlertType
+import javafx.stage.Stage
+import nicolasorteg.gestion1daw.routes.RoutesManager
+import nicolasorteg.gestion1daw.viewmodels.RegisterViewModel
+import org.lighthousegames.logging.logging
 
-class HelloController {
-    @FXML
-    private lateinit var welcomeText: Label
+class RegisterController {
+
+    private val logger = logging()
 
     @FXML
-    private fun onHelloButtonClick() {
-        welcomeText.text = "Welcome to JavaFX Application!"
+    private lateinit var usernameTextField: TextField
+
+    @FXML
+    private lateinit var passwordField: PasswordField
+
+    private val viewModel = RegisterViewModel()
+
+    @FXML
+    fun initialize() {
+        usernameTextField.textProperty().bindBidirectional(viewModel.usernameProperty)
+        passwordField.textProperty().bindBidirectional(viewModel.passwordProperty)
+    }
+
+    @FXML
+    private fun onOkButtonClick() {
+        if (!viewModel.validateCredentials()) {
+            val alert = Alert(AlertType.WARNING)
+            alert.title = "Campos vacíos" // titulo
+            alert.headerText = null
+            alert.contentText = "RELLENA TODOS LOS CAMPOS" // mensaje
+            alert.showAndWait()
+        } else {
+            logger.debug { "✅ Registro finalizado con éxito" }
+            println("Usuario: ${viewModel.getUsername()}")
+            println("Contraseña: ${viewModel.getPassword()}")
+            RoutesManager.initPantallaInicialStage(usernameTextField.scene.window as Stage)
+
+        }
     }
 }
