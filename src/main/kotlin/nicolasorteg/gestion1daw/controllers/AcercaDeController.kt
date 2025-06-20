581957dadb4b57c69c3d4e1f7ddcdae89965e5ad
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-20 23:24:40 +0200

    version inicial acerca de controller

--- src/main/kotlin/nicolasorteg/gestion1daw/controllers/AcercaDeController.kt
+++ src/main/kotlin/nicolasorteg/gestion1daw/controllers/AcercaDeController.kt
@@ -1,4 +1,21 @@
 package nicolasorteg.gestion1daw.controllers
 
+import javafx.fxml.FXML
+import javafx.scene.input.MouseEvent
+import java.awt.Desktop
+import java.net.URI
+
 class AcercaDeController {
+
+    @FXML
+    private fun onGithubClicked(event: MouseEvent) {
+        val url = "https://github.com/nicolasorteg"
+        try {
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(URI(url))
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
