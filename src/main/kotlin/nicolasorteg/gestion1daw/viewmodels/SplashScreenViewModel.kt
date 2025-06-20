90f7c8dde200ccf88255bf5cf43e98dc83bb6dc2
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-20 22:10:38 +0200

    Creamos ViewModel

---
+++ src/main/kotlin/nicolasorteg/gestion1daw/viewmodels/SplashScreenViewModel.kt
@@ -0,0 +1,25 @@
+package nicolasorteg.gestion1daw.viewmodels
+
+import javafx.application.Platform
+import javafx.beans.property.SimpleDoubleProperty
+import javafx.beans.property.SimpleStringProperty
+
+class SplashScreenViewModel {
+
+    val progressProperty = SimpleDoubleProperty(0.0)
+    val mensajeProperty = SimpleStringProperty("")
+
+    fun startLoading(onFinished: () -> Unit) {
+        Thread {
+            for (i in 0..100) {
+                val progress = i / 100.0
+                Platform.runLater {
+                    progressProperty.set(progress)
+                    mensajeProperty.set("Cargando... $i %")
+                }
+                Thread.sleep(50)
+            }
+            Platform.runLater { onFinished() }
+        }.start()
+    }
+}
