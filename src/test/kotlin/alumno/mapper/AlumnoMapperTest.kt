8d6a95c04004f4039638704575a9ee873d281dfb
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-21 06:57:58 +0200

    finalmente test mapper acabado

--- src/test/kotlin/alumno/mapper/AlumnoMapperTest.kt
+++ src/test/kotlin/alumno/mapper/AlumnoMapperTest.kt
@@ -2,44 +2,43 @@ package alumno.mapper
 
 import nicolasorteg.gestion1daw.alumno.dao.AlumnoEntity
 import nicolasorteg.gestion1daw.alumno.dto.AlumnoDto
-import nicolasorteg.gestion1daw.alumno.mappers.toDto
-import nicolasorteg.gestion1daw.alumno.mappers.toEntity
-import nicolasorteg.gestion1daw.alumno.mappers.toModel
+import nicolasorteg.gestion1daw.alumno.mapper.toDto
+import nicolasorteg.gestion1daw.alumno.mapper.toEntity
+import nicolasorteg.gestion1daw.alumno.mapper.toModel
 import nicolasorteg.gestion1daw.alumno.models.Alumno
-import nicolasorteg.gestion1daw.expediente.dao.ExpedienteEntity
-import nicolasorteg.gestion1daw.expediente.dto.ExpedienteDto
-import nicolasorteg.gestion1daw.expediente.model.Expediente
 import nicolasorteg.gestion1daw.modulo.dao.ModuloEntity
 import nicolasorteg.gestion1daw.modulo.dto.ModuloDto
 import nicolasorteg.gestion1daw.modulo.model.Modulo
+import nicolasorteg.gestion1daw.expediente.dao.ExpedienteEntity
+import nicolasorteg.gestion1daw.expediente.dto.ExpedienteDto
+import nicolasorteg.gestion1daw.expediente.model.Expediente
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class AlumnoMapperTest {
 
-    // datos de ejemplo para Modulo
-    private val moduloModel = Modulo(nombre = "Programacion", siglas = "PROG", horas = 200)
-    private val moduloDto = ModuloDto(nombre = "Programacion", siglas = "PROG", horas = 200)
-    private val moduloEntity = ModuloEntity(nombre = "Programacion", siglas = "PROG", horas = 200)
+    // Datos de ejemplo para Modulo
+    private val moduloEntity = ModuloEntity("Matemáticas", "MAT", 100)
+    private val moduloDto = ModuloDto("Matemáticas", "MAT", 100)
+    private val moduloModel = Modulo("Matemáticas", "MAT", 100)
 
-    // datos de ejemplo para Expediente
-    private val calificacionesEjemplo = mapOf("PROG" to 7.5, "BBDD" to 8.0)
-    private val expedienteModel = Expediente(calificaciones = calificacionesEjemplo, notaMedia = 7.75, observaciones = "Buen progreso")
-    private val expedienteDto = ExpedienteDto(calificaciones = calificacionesEjemplo, notaMedia = 7.75, observaciones = "Buen progreso")
-    private val expedienteEntity = ExpedienteEntity(calificaciones = calificacionesEjemplo, notaMedia = 7.75, observaciones = "Buen progreso")
+    // Datos de ejemplo para Expediente
+    private val expedienteEntity = ExpedienteEntity(mapOf("MAT" to 7.5), 7.5, "Observación")
+    private val expedienteDto = ExpedienteDto(mapOf("MAT" to 7.5), 7.5, "Observación")
+    private val expedienteModel = Expediente(mapOf("MAT" to 7.5), 7.5, "Observación")
 
-    // datos de ejemplo para Alumno
-    private val alumnoModel = Alumno(
+    // Datos de ejemplo para Alumno
+    private val alumnoEntity = AlumnoEntity(
         id = 1,
-        nombre = "Juan",
-        apellidos = "Perez",
-        fechaNacimiento = "2000-01-15",
-        edad = 24,
+        nombre = "Nico",
+        apellidos = "Ortega",
+        fechaNacimiento = "2000-01-01",
+        edad = 25,
         nacionalidad = "Española",
-        fechaIncorporacion = "2023-09-01",
-        modulos = listOf(moduloModel),
-        expediente = expedienteModel,
-        notaMedia = 7.75,
+        fechaIncorporacion = "2018-09-01",
+        modulos = listOf(moduloEntity),
+        expediente = expedienteEntity,
+        notaMedia = 7.5,
         faltas = 2,
         retrasos = 1,
         partes = 0
@@ -47,124 +46,99 @@ class AlumnoMapperTest {
 
     private val alumnoDto = AlumnoDto(
         id = 1,
-        nombre = "Juan",
-        apellidos = "Perez",
-        fechaNacimiento = "2000-01-15",
-        edad = 24,
+        nombre = "Nico",
+        apellidos = "Ortega",
+        fechaNacimiento = "2000-01-01",
+        edad = 25,
         nacionalidad = "Española",
-        fechaIncorporacion = "2023-09-01",
+        fechaIncorporacion = "2018-09-01",
         modulos = listOf(moduloDto),
         expediente = expedienteDto,
+        notaMedia = 7.5,
         faltas = 2,
         retrasos = 1,
         partes = 0
     )
 
-    private val alumnoEntity = AlumnoEntity(
+    private val alumnoModel = Alumno(
         id = 1,
-        nombre = "Juan",
-        apellidos = "Perez",
-        fechaNacimiento = "2000-01-15",
-        edad = 24,
+        nombre = "Nico",
+        apellidos = "Ortega",
+        fechaNacimiento = "2000-01-01",
+        edad = 25,
         nacionalidad = "Española",
-        fechaIncorporacion = "2023-09-01",
-        modulos = listOf(moduloEntity),
-        expediente = expedienteEntity,
-        notaMedia = 7.75,
+        fechaIncorporacion = "2018-09-01",
+        modulos = listOf(moduloModel),
+        expediente = expedienteModel,
+        notaMedia = 7.5,
         faltas = 2,
         retrasos = 1,
         partes = 0
     )
 
-    @Test
-    fun alumnoToDto() {
-        val result = alumnoModel.toDto()
-        assertNotNull(result)
-        assertEquals(alumnoDto, result)
-        assertTrue(result is AlumnoDto, "El resultado debe ser de tipo AlumnoDto")
-    }
+    private fun assertAlumnoEquals(expected: Alumno, actual: Alumno) {
+        assertEquals(expected.id, actual.id, "ID no coincide")
+        assertEquals(expected.nombre, actual.nombre, "Nombre no coincide")
+        assertEquals(expected.apellidos, actual.apellidos, "Apellidos no coinciden")
+        assertEquals(expected.fechaNacimiento, actual.fechaNacimiento, "Fecha de Nacimiento no coincide")
+        assertEquals(expected.edad, actual.edad, "Edad no coincide")
+        assertEquals(expected.nacionalidad, actual.nacionalidad, "Nacionalidad no coincide")
+        assertEquals(expected.fechaIncorporacion, actual.fechaIncorporacion, "Fecha de Incorporación no coincide")
+        assertEquals(expected.notaMedia, actual.notaMedia, "Nota Media no coincide")
+        assertEquals(expected.faltas, actual.faltas, "Faltas no coinciden")
+        assertEquals(expected.retrasos, actual.retrasos, "Retrasos no coinciden")
+        assertEquals(expected.partes, actual.partes, "Partes no coinciden")
 
-    @Test
-    fun alumnoEntityToDto() {
-        val result = alumnoEntity.toDto()
-        assertNotNull(result)
-        assertEquals(alumnoDto, result)
-        assertTrue(result is AlumnoDto, "El resultado debe ser de tipo AlumnoDto")
-    }
 
-    @Test
-    fun alumnoDtoToModel() {
-        val result = alumnoDto.toModel()
-        assertNotNull(result)
-        // Comparación propiedad por propiedad
-        assertEquals(alumnoModel.id, result.id)
-        assertEquals(alumnoModel.nombre, result.nombre)
-        assertEquals(alumnoModel.apellidos, result.apellidos)
-        assertEquals(alumnoModel.fechaNacimiento, result.fechaNacimiento)
-        assertEquals(alumnoModel.edad, result.edad)
-        assertEquals(alumnoModel.nacionalidad, result.nacionalidad)
-        assertEquals(alumnoModel.fechaIncorporacion, result.fechaIncorporacion)
-        assertEquals(alumnoModel.modulos.size, result.modulos.size)
-        alumnoModel.modulos.forEachIndexed { index, expectedModulo ->
-            val actualModulo = result.modulos[index]
-            assertEquals(expectedModulo.nombre, actualModulo.nombre)
-            assertEquals(expectedModulo.siglas, actualModulo.siglas)
-            assertEquals(expectedModulo.horas, actualModulo.horas)
+        assertEquals(expected.modulos.size, actual.modulos.size, "El número de módulos no coincide")
+        expected.modulos.forEachIndexed { index, expectedModulo ->
+            val actualModulo = actual.modulos[index]
+            assertEquals(expectedModulo.nombre, actualModulo.nombre, "Nombre de módulo en el índice $index no coincide")
+            assertEquals(expectedModulo.siglas, actualModulo.siglas, "Siglas de módulo en el índice $index no coinciden")
+            assertEquals(expectedModulo.horas, actualModulo.horas, "Horas de módulo en el índice $index no coinciden")
         }
-        assertEquals(alumnoModel.expediente.calificaciones, result.expediente.calificaciones)
-        assertEquals(alumnoModel.expediente.notaMedia, result.expediente.notaMedia)
-        assertEquals(alumnoModel.expediente.observaciones, result.expediente.observaciones)
 
-        assertEquals(alumnoModel.notaMedia, result.notaMedia)
-        assertEquals(alumnoModel.faltas, result.faltas)
-        assertEquals(alumnoModel.retrasos, result.retrasos)
-        assertEquals(alumnoModel.partes, result.partes)
-        assertTrue(result is Alumno, "El resultado debe ser de tipo Alumno")
+
+        assertEquals(expected.expediente.calificaciones, actual.expediente.calificaciones, "Calificaciones del expediente no coinciden")
+        assertEquals(expected.expediente.notaMedia, actual.expediente.notaMedia, "Nota media del expediente no coincide")
+        assertEquals(expected.expediente.observaciones, actual.expediente.observaciones, "Observaciones del expediente no coinciden")
     }
 
-    @Test
-    fun alumnoEntityToModel() {
-        val result = alumnoEntity.toModel()
-        assertNotNull(result)
-        // Comparación propiedad por propiedad
-        assertEquals(alumnoModel.id, result.id)
-        assertEquals(alumnoModel.nombre, result.nombre)
-        assertEquals(alumnoModel.apellidos, result.apellidos)
-        assertEquals(alumnoModel.fechaNacimiento, result.fechaNacimiento)
-        assertEquals(alumnoModel.edad, result.edad)
-        assertEquals(alumnoModel.nacionalidad, result.nacionalidad)
-        assertEquals(alumnoModel.fechaIncorporacion, result.fechaIncorporacion)
-        assertEquals(alumnoModel.modulos.size, result.modulos.size)
-        alumnoModel.modulos.forEachIndexed { index, expectedModulo ->
-            val actualModulo = result.modulos[index]
-            assertEquals(expectedModulo.nombre, actualModulo.nombre)
-            assertEquals(expectedModulo.siglas, actualModulo.siglas)
-            assertEquals(expectedModulo.horas, actualModulo.horas)
-        }
-        assertEquals(alumnoModel.expediente.calificaciones, result.expediente.calificaciones)
-        assertEquals(alumnoModel.expediente.notaMedia, result.expediente.notaMedia)
-        assertEquals(alumnoModel.expediente.observaciones, result.expediente.observaciones)
 
-        assertEquals(alumnoModel.notaMedia, result.notaMedia)
-        assertEquals(alumnoModel.faltas, result.faltas)
-        assertEquals(alumnoModel.retrasos, result.retrasos)
-        assertEquals(alumnoModel.partes, result.partes)
-        assertTrue(result is Alumno, "El resultado debe ser de tipo Alumno")
+
+    @Test
+    fun `AlumnoEntity toDto y back`() {
+        val dto = alumnoEntity.toDto()
+
+        assertEquals(alumnoDto, dto, "La conversión de AlumnoEntity a AlumnoDto no es correcta.")
+
+        val entityBack = dto.toEntity()
+
+        assertEquals(alumnoEntity, entityBack, "La conversión de AlumnoDto a AlumnoEntity no es correcta.")
     }
 
-    @Test
-    fun alumnoToEntity() {
-        val result = alumnoModel.toEntity()
-        assertNotNull(result)
-        assertEquals(alumnoEntity, result)
-        assertTrue(result is AlumnoEntity, "El resultado debe ser de tipo AlumnoEntity")
-    }
+
 
     @Test
-    fun AlumnoDtoToEntity() {
-        val result = alumnoDto.toEntity()
-        assertNotNull(result)
-        assertEquals(alumnoEntity, result)
-        assertTrue(result is AlumnoEntity, "El resultado debe ser de tipo AlumnoEntity")
+    fun `AlumnoDto toModel y back`() {
+        val model = alumnoDto.toModel()
+
+        assertAlumnoEquals(alumnoModel, model)
+
+        val dtoBack = model.toDto()
+
+        assertEquals(alumnoDto, dtoBack, "La conversión de Alumno (model) a AlumnoDto no es correcta.")
+    }
+
+
+    @Test
+    fun `AlumnoEntity toModel y back`() {
+        val model = alumnoEntity.toModel()
+
+        assertAlumnoEquals(alumnoModel, model)
+
+        val entityBack = model.toEntity()
+
+        assertEquals(alumnoEntity, entityBack, "La conversión de Alumno (model) a AlumnoEntity no es correcta.")
     }
 }
