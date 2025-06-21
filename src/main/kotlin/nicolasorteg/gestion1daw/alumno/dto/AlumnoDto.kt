02671e23c4d59e943452aec831466d0879faf2ba
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-21 05:32:10 +0200

    ajustes

---
+++ src/main/kotlin/nicolasorteg/gestion1daw/alumno/dto/AlumnoDto.kt
@@ -0,0 +1,19 @@
+package nicolasorteg.gestion1daw.alumno.dto
+
+import nicolasorteg.gestion1daw.modulo.dto.ModuloDto
+import nicolasorteg.gestion1daw.expediente.dto.ExpedienteDto
+
+data class AlumnoDto(
+    val id: Int,
+    val nombre: String,
+    val apellidos: String,
+    val fechaNacimiento: String,
+    val edad: Int,
+    val nacionalidad: String,
+    val fechaIncorporacion: String,
+    val modulos: List<ModuloDto>,
+    val expediente: ExpedienteDto,
+    val faltas: Int,
+    val retrasos: Int,
+    val partes: Int
+)
