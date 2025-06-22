5df84e1276557c37446a221876fd47f9b3d8c0d2
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-22 16:58:48 +0200

    funcion save implementada

--- src/main/kotlin/nicolasorteg/gestion1daw/alumno/repository/AlumnoRepositoryImpl.kt
+++ src/main/kotlin/nicolasorteg/gestion1daw/alumno/repository/AlumnoRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package nicolasorteg.gestion1daw.alumno.repository
 
 import nicolasorteg.gestion1daw.alumno.dao.AlumnoDao
+import nicolasorteg.gestion1daw.alumno.mappers.toEntity
+import nicolasorteg.gestion1daw.alumno.mappers.toModel
 import nicolasorteg.gestion1daw.alumno.models.Alumno
 import org.lighthousegames.logging.logging
 
@@ -10,7 +12,10 @@ class AlumnoRepositoryImpl(
     private val logger = logging()
 
     override fun save(objeto: Alumno): Alumno {
-        TODO("Not yet implemented")
+        val alumnoEntity = objeto.toEntity()
+        val generatedId = dao.save(alumnoEntity)
+        val savedEntity = dao.findById(generatedId) ?: throw IllegalStateException("Error al guardar el alumno.")
+        return savedEntity.toModel()
     }
 
     override fun getAll(): List<Alumno> {
