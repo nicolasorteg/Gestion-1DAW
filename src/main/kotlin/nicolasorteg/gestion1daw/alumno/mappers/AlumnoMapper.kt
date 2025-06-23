e892fbe6b12eee0c78a46627f6c30b283de1420a
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-23 02:32:18 +0200

    mapper acabado

--- src/main/kotlin/nicolasorteg/gestion1daw/alumno/mappers/AlumnoMapper.kt
+++ src/main/kotlin/nicolasorteg/gestion1daw/alumno/mappers/AlumnoMapper.kt
@@ -1,4 +1,4 @@
-package nicolasorteg.gestion1daw.alumno.mapper
+package nicolasorteg.gestion1daw.alumno.mappers
 
 import nicolasorteg.gestion1daw.alumno.dao.AlumnoEntity
 import nicolasorteg.gestion1daw.alumno.dto.AlumnoDto
