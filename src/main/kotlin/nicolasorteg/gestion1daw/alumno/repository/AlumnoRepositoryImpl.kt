41285cd7fd653df4b3f5cc75633620f8f1717b81
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-23 02:30:36 +0200

    subido repository con demora

--- src/main/kotlin/nicolasorteg/gestion1daw/alumno/repository/AlumnoRepositoryImpl.kt
+++ src/main/kotlin/nicolasorteg/gestion1daw/alumno/repository/AlumnoRepositoryImpl.kt
@@ -1,14 +1,15 @@
 package nicolasorteg.gestion1daw.alumno.repository
 
 import nicolasorteg.gestion1daw.alumno.dao.AlumnoDao
+import nicolasorteg.gestion1daw.alumno.models.Alumno
 import nicolasorteg.gestion1daw.alumno.mappers.toEntity
 import nicolasorteg.gestion1daw.alumno.mappers.toModel
-import nicolasorteg.gestion1daw.alumno.models.Alumno
 import org.lighthousegames.logging.logging
 
 class AlumnoRepositoryImpl(
     private val dao: AlumnoDao
 ): AlumnoRepository<Alumno, Int> {
+
     private val logger = logging()
 
     override fun save(objeto: Alumno): Alumno {
@@ -19,18 +20,46 @@ class AlumnoRepositoryImpl(
     }
 
     override fun getAll(): List<Alumno> {
-        TODO("Not yet implemented")
+        logger.debug { "Recuperando todos los alumnos" }
+        return dao.findAll().map { it.toModel() }
     }
 
     override fun delete(id: Int): Alumno? {
-        TODO("Not yet implemented")
+        logger.debug { "Eliminando alumno con id = $id" }
+        val alumno = dao.findById(id)?.toModel()
+        if (alumno != null) {
+            dao.delete(id)
+        }
+        return alumno
     }
 
     override fun update(objeto: Alumno, id: Int): Alumno? {
-        TODO("Not yet implemented")
+        logger.debug { "Actualizando alumno con id = $id" }
+
+        val alumnoExistente = dao.findById(id) ?: return null
+
+        val alumnoActualizado = Alumno(
+            id = id,
+            nombre = objeto.nombre,
+            apellidos = objeto.apellidos,
+            fechaNacimiento = objeto.fechaNacimiento,
+            edad = objeto.edad,
+            nacionalidad = objeto.nacionalidad,
+            fechaIncorporacion = objeto.fechaIncorporacion,
+            modulos = objeto.modulos,
+            expediente = objeto.expediente,
+            notaMedia = objeto.notaMedia,
+            faltas = objeto.faltas,
+            retrasos = objeto.retrasos,
+            partes = objeto.partes
+        ).toEntity()
+
+        val updatedRows = dao.update(alumnoActualizado)
+        return if (updatedRows > 0) dao.findById(id)?.toModel() else null
     }
 
     override fun getById(id: Int): Alumno? {
-        TODO("Not yet implemented")
+        logger.debug { "Buscando alumno con id = $id" }
+        return dao.findById(id)?.toModel()
     }
 }
