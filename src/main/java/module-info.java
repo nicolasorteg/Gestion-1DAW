2dcf31c2b40a5b92340d25641e9ce2c7d252e2b9
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-06-23 02:28:12 +0200

    algun pequeño cambio para los test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,15 +2,24 @@ module nicolasorteg.gestion1daw {
     requires javafx.controls;
     requires javafx.fxml;
     requires kotlin.stdlib;
+
+    // result
     requires kotlin.result.jvm;
 
     // Logger
     requires logging.jvm;
     requires org.slf4j;
+
     requires java.desktop;
+
+    // jdbi
     requires org.jdbi.v3.sqlobject.kotlin;
     requires org.jdbi.v3.sqlobject;
     requires org.jdbi.v3.core;
+
+
+    opens nicolasorteg.gestion1daw.alumno.repository to org.mockito.mockito.core, io.mockk;
+    opens nicolasorteg.gestion1daw.alumno.service to org.mockito.mockito.core, io.mockk;
 
     opens nicolasorteg.gestion1daw to javafx.fxml;
     opens nicolasorteg.gestion1daw.common.controllers to javafx.fxml;
